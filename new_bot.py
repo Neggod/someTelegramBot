6aaafa9a31e9e0d807c21d7c9c9b0c9f82ed4138
Neggod <neggod@ya.ru>
2019-11-29 23:42:13 +0800

    Add callback menu

--- new_bot.py
+++ new_bot.py
@@ -1,8 +1,10 @@
 import telebot
+import re
+
 
 TOKEN = '773428065:AAFczy9ybU3d7cJzvmpaVscLSn6miZnd3vI'
 bot = telebot.TeleBot(TOKEN)
-
+hashtag_pattern = re.compile("(^|\s)#([^\d&%$_-]\S{2,49})\b")
 
 @bot.message_handler(commands=['start', 'help'])
 def hello_im_bot(message):
@@ -13,8 +15,20 @@ def hello_im_bot(message):
 @bot.message_handler(content_types=['text'])
 def echo_text(message):
 	text = message.text.replace('\n', '\u2800\n')
-	bot.send_message(message.chat.id, text)
+	len_text = len(text)
+	hashtags = len(re.findall(hashtag_pattern, text))
+	markup = telebot.types.InlineKeyboardMarkup()
+	button = telebot.types.InlineKeyboardButton("{0} из 2200; # - {1}".format(len_text, hashtags),
+												callback_data=("instatext", len_text, hashtags))
+	markup.add(button)
+	bot.send_message(message.chat.id, text, reply_markup=markup)
 
+
+@bot.callback_query_handler(func=lambda call: True)
+def query_handler(call):
+	if call.data[0] == 'instatext':
+		bot.answer_callback_query(callback_query_id=call.id, text='{0} из 2200; # - {1}'.format(*call.data[1:]))
 
 if __name__ == '__main__':
 	bot.polling(none_stop=True, timeout=100)
+
