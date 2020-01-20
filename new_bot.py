3487c89801683254c6632c0bd5ca7827a389257d
Neggod <neggod@ya.ru>
2020-01-20 18:04:16 +0800

    Comment 1000 subscribers check for test

--- new_bot.py
+++ new_bot.py
@@ -212,10 +212,10 @@ def check_user_group(chat_id, user_id, link, new_channel=True):
             chat_ = bot.get_chat(chat_id)
             print(chat_)
             total = bot.get_chat_members_count(chat_.id)
-            if total < 1000:
-                worker.users[user_id].clear()
-                return "В вашем канале меньше 1000 подписчиков.", big_btn
-            if not chat_.description or (chat_.description and not worker.users[user_id].username in chat_.description):
+            # if total < 1000:
+            #     worker.users[user_id].clear()
+            #     return "В вашем канале меньше 1000 подписчиков.", big_btn
+            if not chat_.description or (not worker.users[user_id].username in chat_.description):
 
                 print(f"USER {worker.users[user_id].username} NOT IN DESCRIPTION OF CHAT {chat_.title}")
                 for chat_member in bot.get_chat_administrators(chat_.id):
